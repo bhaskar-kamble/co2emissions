17c431e5be84e95439b27825405657bf7f22cc39
bhaskar-kamble <kbhaskar.iitk@gmail.com>
2019-11-04 19:54:45 +0100

    201911041954

--- Berlin/FiguresForSenate/saveFigureTeil2.R
+++ Berlin/FiguresForSenate/saveFigureTeil2.R
@@ -1,4 +1,4 @@
-saveFigureTeil2 <- function(plotFig=TRUE,
+saveFigureTeil2 <- function(plotFig=FALSE,
                        gtypeForFig,
                        section=NULL,
                        subsection=NULL,
@@ -9,38 +9,40 @@ saveFigureTeil2 <- function(plotFig=TRUE,
                        bezirk_index,
                        whatToSave = last_plot()) {
   
-  bezirk_list  <- c(
-    "charlottenburg_wilmersdorf",
-    "friedrichshain_kreuzberg",
-    "lichtenberg",
-    "marzahn_hellersdorf",
-    "mitte",
-    "neukoelln",
-    "pankow"    ,             
-    "reinickendorf",
-    "spandau",
-    "steglitz_zehlendorf" ,
-    "tempelhof_schoeneberg",
-    "treptow_koepenick"                              
-  )
-
-  pathToFile <- "/home/kbhaskar/Github_Repos/co2emissions/Berlin/FiguresForSenate/Teil2/"
-  pathToFile <- paste0(pathToFile , bezirk_list[bezirk_index] , "/")
+  if (plotFig) {
   
-  figName <- paste0(gtypeForFig,"_Section_",section,"_",subsection,"_",subsubsection,"_fig_",figIndex,".png")
-  figName <- paste0(pathToFile , figName)
-  if (!is.null(figWidth) & !is.null(figHeight)) {
-    ggsave(figName , 
-           plot = whatToSave,
-           width = figWidth , 
-           height=figHeight)
+    bezirk_list  <- c(
+      "charlottenburg_wilmersdorf",
+      "friedrichshain_kreuzberg",
+      "lichtenberg",
+      "marzahn_hellersdorf",
+      "mitte",
+      "neukoelln",
+      "pankow"    ,             
+      "reinickendorf",
+      "spandau",
+      "steglitz_zehlendorf" ,
+      "tempelhof_schoeneberg",
+      "treptow_koepenick"                              
+    )
+    
+    pathToFile <- "/home/kbhaskar/Github_Repos/co2emissions/Berlin/FiguresForSenate/Teil2/"
+    pathToFile <- paste0(pathToFile , bezirk_list[bezirk_index] , "/")
+    
+    figName <- paste0(gtypeForFig,"_Section_",section,"_",subsection,"_",subsubsection,"_fig_",figIndex,".png")
+    figName <- paste0(pathToFile , figName)
+    if (!is.null(figWidth) & !is.null(figHeight)) {
+      ggsave(figName , 
+             plot = whatToSave,
+             width = figWidth , 
+             height=figHeight)
+    }
+    if (is.null(figWidth) & is.null(figHeight)) {
+      ggsave(figName , plot = whatToSave)
+    }
+    if ((is.null(figWidth) & !is.null(figHeight))|((!is.null(figWidth) & is.null(figHeight))))  {
+      stop("either specify BOTH height and width, or specify none")
+    }
   }
-  if (is.null(figWidth) & is.null(figHeight)) {
-    ggsave(figName , plot = whatToSave)
-  }
-  if ((is.null(figWidth) & !is.null(figHeight))|((!is.null(figWidth) & is.null(figHeight))))  {
-    stop("either specify BOTH height and width, or specify none")
-  }
-  
   return(figIndex+1)
 }
